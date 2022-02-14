8d1c73a9a507ef8958ea5dd35e98d607ed3274ab
AdelleMcateer <adellemaca@gmail.com>
2022-02-14 18:00:35 +0000

    Components and basic pages added.

---
+++ client/src/components/DefaultLayout.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function DefaultLayout(props) {
+  return (
+    <div>
+
+        <div className='header bs1'>
+            <div className='d-flex justify-content-between'>
+
+                <h1>WoofDayCare</h1>
+
+                <button>user</button>
+
+            </div>
+        </div>
+        <div className='content'>
+            {props.children}
+        </div>
+    </div>
+  )
+}
+
+export default DefaultLayout
---
+++ client/src/pages/BookingSitter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import DefaultLayout from '../components/DefaultLayout'
+
+function BookingSitter() {
+  return (
+    <DefaultLayout>
+    <h1>BookingSitter</h1>
+    </DefaultLayout>
+
+  )
+}
+
+export default BookingSitter
