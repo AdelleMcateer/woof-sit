26e5a490173e7edb91087016c35e43667f7011df
AdelleMcateer <adellemaca@gmail.com>
2022-02-14 17:59:09 +0000

    Layout and basic routes implemented for front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import BookingSitter from './pages/BookingSitter';
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <BrowserRouter>
+      <Route path='/' exact component={Home} />
+      <Route path='/Login' exact component={Login} />
+      <Route path='/register' exact component={Register} />
+      <Route path='/bookingsitter' exact component={BookingSitter} />
+      
+      </BrowserRouter>
+
     </div>
   );
 }
