0aacb9bb368aede9c12428b4856d52758f57cb8c
AdelleMcateer <adellemaca@gmail.com>
2022-02-17 07:11:34 +0000

    Login & Register page design.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import { Row, Col, Form, Input} from 'antd'
+import { Link } from 'react-router-dom'
 
 function Login() {
   return (
-    <div>
-        <h1>Login</h1>
+    <div className='login'>
+
+        <Row gutter={16} className='d-flex aligh-items-center' >
+
+          <Col lg={16} style={{positon: 'relative'}}>
+            <img src='https://cdn.pixabay.com/photo/2019/04/10/23/51/dog-4118585_960_720.jpg'></img>
+            <h1 className='login-logo'>Woof Doggy Day Care</h1>
+          </Col>
+          <Col lg={8} className='text-left p-5'>
+          <Form layout='vertical' className='login-form p-5'>
+            <h1>Login</h1>
+            <hr />
+            <Form.Item name='username' label='Username' rules={[{required: true}]}>
+              <Input></Input>
+            </Form.Item>
+            <Form.Item name='password' label='Password' rules={[{required: true}]}>
+              <Input></Input>
+            </Form.Item>
+
+            <button className='btn1 mt-2 mb-3'>Login</button>
+            <br />
+            <Link to='/register'>Click here to Register</Link>
+            
+          </Form>
+          </Col>
+
+        </Row>
+
     </div>
   )
 }
