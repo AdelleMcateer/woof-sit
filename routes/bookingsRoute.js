8ff361250648ddb259881497b504f0e9d4d10ec8
AdelleMcateer <adellemaca@gmail.com>
2022-02-20 20:16:20 +0000

    Stripe implemented on the back-end. Transactions visible in database and Stripe account.

--- routes/bookingsRoute.js
+++ routes/bookingsRoute.js
@@ -2,19 +2,42 @@ const express = require("express");
 const router = express.Router();
 const Booking = require("../models/bookingModel");
 const Sitter = require("../models/sitterModel");
+const stripe = require("stripe")("my_PRIVATE_stripekey");
+const { v4: uuidv4 } = require("uuid");
 
 router.post("/booksitter", async (req, res) => {
-  req.body.transactionId = "1234";
+  const { token } = req.body;
   try {
-    const newbooking = new Booking(req.body);
-    await newbooking.save();
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id,
+    });
 
-    //Linking bookied time slots to sitter
-    const sitter = await Sitter.findOne({ _id: req.body.sitter });
-    sitter.bookedTimeSlots.push(req.body.bookedTimeSlots);
+    const payment = await stripe.charges.create(
+      {
+        amount: req.body.totalAmount * 100,
+        currency: "eur",
+        customer: customer.id,
+        receipt_email: token.email,
+      },
+      {
+        idempotencyKey: uuidv4(),
+      }
+    );
 
-    await sitter.save();
-    res.send("Your booking was successful");
+    if (payment) {
+      req.body.transactionId = payment.source.id;
+      const newbooking = new Booking(req.body);
+      await newbooking.save();
+      const sitter = await Sitter.findOne({ _id: req.body.sitter });
+      console.log(req.body.sitter);
+      sitter.bookedTimeSlots.push(req.body.bookedTimeSlots);
+
+      await sitter.save();
+      res.send("Your booking is successfull");
+    } else {
+      return res.status(400).json(error);
+    }
   } catch (error) {
     console.log(error);
     return res.status(400).json(error);
