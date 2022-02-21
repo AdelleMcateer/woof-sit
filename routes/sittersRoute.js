393fad903524441f4c123040ef0aded8002ac91e
AdelleMcateer <adellemaca@gmail.com>
2022-02-21 12:07:32 +0000

    Admin add new car API implemented and functioning. Details visible in MongoDB.

--- routes/sittersRoute.js
+++ routes/sittersRoute.js
@@ -1,14 +1,24 @@
+const { response } = require("express");
 const express = require("express");
 const router = express.Router();
 const Sitter = require("../models/sitterModel");
 
 router.get("/getallsitters", async (req, res) => {
-    try {
-      const sitters = await Sitter.find();
-      res.send(sitters);
-    } catch (error) {
-      return res.status(400).json(error);
-    }
-  });
+  try {
+    const sitters = await Sitter.find();
+    res.send(sitters);
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+});
 
-  module.exports = router;
+router.post("/addsitter", async (req, res) => {
+  try {
+    const newsitter = new Sitter(req.body);
+    await newsitter.save();
+    res.send("Sitter added successfully");
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+});
+module.exports = router;
