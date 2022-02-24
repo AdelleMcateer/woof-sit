5d0dbf5fa42978826efd31004a344e7d8ee9c33f
AdelleMcateer <adellemaca@gmail.com>
2022-02-24 16:35:19 +0000

    Testing deployment

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -19,8 +19,8 @@ router.post("/login", async (req, res) => {
 
 router.post("/register", async (req, res) => {
   try {
-    const newUser = new User(req.body);
-    await newUser.save();
+    const newuser = new User(req.body);
+    await newuser.save();
     res.send("User registered successfully");
   } catch (error) {
     return res.status(400).json(error);
