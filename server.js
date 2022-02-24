d1be5d8cc76196f769e57f3f5fe8e5c9e8118729
AdelleMcateer <adellemaca@gmail.com>
2022-02-24 07:14:57 +0000

    Fixes for deployment.

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.use("/api/users/", require("./routes/usersRoute"));
 app.use("/api/bookings/", require("./routes/bookingsRoute"));
 
 const path = require("path");
-const res = require("express/lib/response");
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(client / build));
+  app.use("/", express.static("client/build"));
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client/build/index.html"));
   });
