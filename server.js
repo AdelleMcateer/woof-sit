1ce0aee2d21a31ca219dc2409754e8698a1c7887
AdelleMcateer <adellemaca@gmail.com>
2022-02-24 14:08:26 +0000

    Reverting to release commit due to errors and unresolvable issues.

--- server.js
+++ server.js
@@ -2,17 +2,20 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const dbConnection = require("./db");
+//const dbConnection = process.env.db;
 app.use(express.json());
+//const uri = process.env.MONGODB_URI;
+//const dbConnection = process.env.MONGODB_URI;
 
 app.use("/api/sitters/", require("./routes/sittersRoute"));
 app.use("/api/users/", require("./routes/usersRoute"));
 app.use("/api/bookings/", require("./routes/bookingsRoute"));
 
 const path = require("path");
-const res = require("express/lib/response");
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(client / build));
+  app.use("/", express.static("client/build"));
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client/build/index.html"));
   });
