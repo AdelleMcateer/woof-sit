554ddf1c878ae69c6290db614366ad8cc1aa4753
AdelleMcateer <adellemaca@gmail.com>
2022-02-24 20:52:25 +0000

    Deployment test.
    Code tidy up, correcting commit messages.

--- server.js
+++ server.js
@@ -9,14 +9,15 @@ app.use("/api/users/", require("./routes/usersRoute"));
 app.use("/api/bookings/", require("./routes/bookingsRoute"));
 
 const path = require("path");
-const res = require("express/lib/response");
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(client / build));
+  app.use("/", express.static("client/build"));
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client/build/index.html"));
   });
 }
 
 app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Node JS Server started on Port ${port}!`));
+
+app.listen(port, () => console.log(`Node JS Server Started in Port ${port}`));
