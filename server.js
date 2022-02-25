e7ec0bd859a749e9a3c354e37d3cc225f4a528de
AdelleMcateer <adellemaca@gmail.com>
2022-02-25 07:26:14 +0000

    Removing commented out lines.

--- server.js
+++ server.js
@@ -19,19 +19,8 @@ app.get("*", function (req, res) {
   res.sendFile(path.resolve(__dirname, "client/build/index.html"));
 });
 
-/*const path = require("path");
-
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-  });
-}
-
+/*
 app.get("/", (req, res) => res.send("Hello World!"));
-
-app.listen(port, () => console.log(`Node JS Server Started in Port ${port}`));
 */
 
 // catch 404 and forward to error handler
