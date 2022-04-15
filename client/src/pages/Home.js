e0c6176641ab440b71cb6d618b81b8dac672a9f6
AdelleMcateer <adellemaca@gmail.com>
2022-04-15 14:17:13 +0100

    Added UML diagram. Updated security vetting process on landing page.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -27,14 +27,18 @@ function Home() {
         <Col lg={8} className="text-left p-5">
           <Form layout="vertical" className="login-form p-5">
             <h1>Welcome to WoofSit</h1>
-            <Form.Item label="Find Sitters and Owners in your area!"></Form.Item>
-            <Form.Item label="For the safety of our pets and owners please register to access more!"></Form.Item>
+            <Form.Item label="Find Sitters and Owners in your area!"
+            ></Form.Item>
+            <Form.Item label="For the safety of our pets and owners please register to access!
+            "></Form.Item>
+            <Form.Item label="Please email a copy of your idenitfication to woofsit@gmail.com. 
+            Unverified users will be removed after 7 days."></Form.Item>
             <Link to="/register">Click here to register</Link>
             <hr />
             <div>
               <br />
               <h1>Testimonials</h1>
-    
+             
             </div>
           </Form>
         </Col>
